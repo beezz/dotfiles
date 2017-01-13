f63b69016330ecbd8aa0c814d48411e7a8a7da3c
Michal Kuffa <michal.kuffa@gmail.com>
2017-01-13 23:15:44 +0100

    Update dp layout for t430

--- config/x/layouts/lvds-dp.sh
+++ config/x/layouts/lvds-dp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+
 xrandr \
-	--output VGA-0 --off \
-	--output LVDS-0 --mode 1600x900 --pos 2560x540 --rotate normal \
-	--output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-	--output DP-5 --off \
-	--output DP-4 --off \
-	--output DP-2 --off \
-	--output DP-1 --off \
-	--output DP-0 --off
+	--output LVDS1 --mode 1600x900 --pos 2560x540 --rotate normal \
+	--output DP2 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+	--output VGA1 --off \
+	--output DP1 --off \
+	--output HDMI1 --off \
+	--output HDMI2 --off \
+	--output HDMI3 --off \
