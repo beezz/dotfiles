9636c6c6ccb369cdf2717fdcecb21d66c71b712d
Michal Kuffa <michal.kuffa@gmail.com>
2017-01-17 23:52:29 +0100

    Rename test to xinitrc

--- config/x/test_xinitrc.py
+++ config/x/test_xinitrc.py
@@ -8,7 +8,7 @@ def current_directory():
     return os.path.abspath(os.path.dirname(__file__))
 
 
-def test_tmuxconf_link(File, current_directory):
+def test_xinitrc_link(File, current_directory):
     zshrc = File(os.path.expanduser('~/.xinitrc'))
     assert zshrc.is_symlink
     assert zshrc.linked_to == os.path.join(current_directory, '.xinitrc')
